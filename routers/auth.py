b0064b05a830736a1fb0349c67033d706eab4145
Branko <olujic.branko@gmail.com>
2022-07-11 08:37:56 +0200

    login and register corrections

--- routers/auth.py
+++ routers/auth.py
@@ -185,6 +185,8 @@ async def login_for_access_token(response: Response,
                                  form_data: OAuth2PasswordRequestForm = Depends(),
                                  db: Session = Depends(get_db)):
     user = authenticate_user(form_data.username, form_data.password, db)
+    if not user:
+        return None
     token_expires = timedelta(minutes=60)
     token = create_access_token(user.username, user.email, user.id, expires_delta=token_expires)
     response.set_cookie(key="access_token", value=token, httponly=True)
