81038c7433f23396a7c5cfd049143055b14f7357
Branko <olujic.branko@gmail.com>
2022-07-11 08:51:50 +0200

    database

--- database.py
+++ database.py
@@ -14,12 +14,12 @@ DATABASE_PASSWORD = os.getenv("DATABASE_PASSWORD", "root")
 DATABASE_HOST = os.getenv("DATABASE_HOST", "localhost")
 DATABASE_PORT = os.getenv("DATABASE_PORT", "5432")
 
-# SQLALCHEMY_DATABASE_URL = os.environ.get('DATABASE_URL')
-# if SQLALCHEMY_DATABASE_URL.startswith('postgres://'):
-#     SQLALCHEMY_DATABASE_URL = SQLALCHEMY_DATABASE_URL.replace('postgres://', 'postgresql://', 1)
-DATABASE_URL = f"postgresql://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_NAME}"
+SQLALCHEMY_DATABASE_URL = os.environ.get('DATABASE_URL')
+if SQLALCHEMY_DATABASE_URL.startswith('postgres://'):
+    SQLALCHEMY_DATABASE_URL = SQLALCHEMY_DATABASE_URL.replace('postgres://', 'postgresql://', 1)
+# DATABASE_URL = f"postgresql://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_NAME}"
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
