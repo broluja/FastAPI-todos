90f8764b05b6c36d4d80d8ba912f0d09962e8852
Branko <olujic.branko@gmail.com>
2022-07-11 12:35:06 +0200

    database

--- database.py
+++ database.py
@@ -3,16 +3,13 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-from dotenv import load_dotenv
-
-load_dotenv()
 
 
-DATABASE_NAME = os.getenv("DATABASE_NAME", "todo")
-DATABASE_USER = os.getenv("DATABASE_USER", "root")
-DATABASE_PASSWORD = os.getenv("DATABASE_PASSWORD", "root")
-DATABASE_HOST = os.getenv("DATABASE_HOST", "localhost")
-DATABASE_PORT = os.getenv("DATABASE_PORT", "5432")
+# DATABASE_NAME = os.getenv("DATABASE_NAME", "todo")
+# DATABASE_USER = os.getenv("DATABASE_USER", "root")
+# DATABASE_PASSWORD = os.getenv("DATABASE_PASSWORD", "root")
+# DATABASE_HOST = os.getenv("DATABASE_HOST", "localhost")
+# DATABASE_PORT = os.getenv("DATABASE_PORT", "5432")
 
 SQLALCHEMY_DATABASE_URL = os.environ.get('DATABASE_URL')
 if SQLALCHEMY_DATABASE_URL.startswith('postgres://'):
